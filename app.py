2422e129801db28166b8a36d2fa0e54863604788
Python-Nath <nath63.dev@laposte.net>
2026-09-23 16:50:53 +0200

    Upgrading app.py finak v1 and create uploads folder to store the revisions notes

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 # I import the flask module for manage the API
-from flask import Flask, request
+from flask import Flask, request, send_file
+import os
+import uuid
+import json
 
 # I import the werkzeug module to verify the integrity of the filename of the file
 from werkzeug.utils import secure_filename 
 
 # Initialisation of the API
-app = Flask(__main__)
+app = Flask(__name__)
 
 # Here the definition for save the uploaded file
 # @Aloijsjimmyargilenilsfranckgeorge you need to verify in the html code page that all the elements (title, author, desc, file) are completed by the user and to send title, author, desc in json format and that the file is in type file
@@ -16,21 +19,47 @@ def upload():
 	title = data.get("titre")
 	desc = data.get("desc")
 	author = data.get("author")
+	path = data.get("path")
 	
 	file = request.files.get("file")
-	filename = secure_filename(file.filename)
+
+	data_json = {
+		"title": title,
+		"desc": desc,
+		"author": author,
+		"path": path
+	}
 	
-	# Here the logic to save the file
+	uuid_str = str(uuid.uuid4())
+	filename = secure_filename(file.filename)
+
+	filename = f"{uuid_str}_{filename}"
+	data_json = {
+			"title": title,
+			"desc": desc,
+			"author": author,
+			"path": path,
+			"name": filename,
+			"stars": 0
+		}
+	file.save(os.path.join("uploads", filename))
+	
 	
 	return "OK", 200
 
 # @Aloijsjimmyargilenilsfranckgeorge you need to verify in the html code page that the element filename are completed by the user and to send it in json format
 @app.route("/download", methods=['POST'])
-def upload():
+def download():
 	data = request.get_json()
 	filename = data.get("filename")
 	
-	# Here the logic to retrive and download the file
+	check_file = os.path.join("uploads", filename)
+	if not os.path.exists(check_file):
+		return "File not found", 404
 	
-	return "OK", 200
+	return send_from_directory("uploads", filename, as_attachment=True, download_name=filename.split("_", 1)[1])
+
+if __name__ == "__main__":
+	os.mkdir("uploads") if not os.path.exists("uploads") else None
+	app.run(host="0.0.0.0", port=8080, debug=False)
 
